e30e1cbe31da97a0fd46ef48c1ac294c73310254
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2022-10-13 14:45:24 -0600

    assignment 8 work

--- Assignments/Assignment 8/main.js
+++ Assignments/Assignment 8/main.js
@@ -3,20 +3,9 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 let storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-let insertX = [
-    "Willy the Goblin",
-    "Big Daddy",
-    "Father Christmas"];
-
-let insertY = [    
-    "the soup kitchen",
-    "Disneyland",
-    "the White House"];
-
-let insertZ = [
-    "spontaneously combusted",
-    "melted into a puddle on the sidewalk",
-    "turned into a slug and crawled away"];
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 let newStory = storyText;
 let xItem = randomValueFromArray(insertX);
@@ -24,8 +13,6 @@ let yItem = randomValueFromArray(insertY);
 let zItem = randomValueFromArray(insertZ);
 
 newStory = newStory.replaceAll(":insertx:", xItem) .replace(":inserty:", yItem) .replace(":insertz:", zItem);
-newStory = newStory.replace(":inserty;", yItem);
-NewStory = newStory.replace(":insertz:", zItem);
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
@@ -38,14 +25,15 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("bob", name);
+    newStory = newStory.replace("Bob", name);
 
   }
 
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/14) + " stone";
     const temperature =  Math.round((94-32)*(5/9)) + " centrigrade";
-    newStory = newStory.replace("300 farenheit", weight);
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 farenheit", temperature);
     
   }
 
