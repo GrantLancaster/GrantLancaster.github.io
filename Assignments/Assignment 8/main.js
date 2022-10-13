2889c55413ffc331a40fc3f39f854b90e965e02e
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2022-10-13 12:18:35 -0600

    Assignment 8

---
+++ Assignments/Assignment 8/main.js
@@ -0,0 +1,54 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+let storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+let insertX = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"];
+
+let insertY = [    
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"];
+
+let insertZ = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away"];
+
+let newStory = storyText;
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+newStory = newStory.replaceAll(":insertx:", xItem) .replace(":inserty:", yItem) .replace(":insertz:", zItem);
+newStory = newStory.replace(":inserty;", yItem);
+NewStory = newStory.replace(":insertz:", zItem);
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace("bob", name);
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300/14) + " stone";
+    const temperature =  Math.round((94-32)*(5/9)) + " centrigrade";
+    newStory = newStory.replace("300 farenheit", weight);
+    
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
