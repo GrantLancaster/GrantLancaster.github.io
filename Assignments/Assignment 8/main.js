70fbafd2918077fb49f564f7519d99a043e9d653
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2022-10-14 13:01:05 -0600

    Assignment 8
    Changed some of the array content to allow for custom stories

--- Assignments/Assignment 8/main.js
+++ Assignments/Assignment 8/main.js
@@ -3,9 +3,9 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 let storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-let insertY = ["the soup kitchen", "Disneyland", "the White House"];
-let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+let insertX = ["Jack Johnson", "Little Sister", "Krampus"];
+let insertY = ["the Olive Garden", "Didney Worl!!!", "Hogwarts School of Witchraft and Wizardry"];
+let insertZ = ["fistfaught a gorilla", "hardened into stone", "Waved and said Hello"];
 
 let newStory = storyText;
 let xItem = randomValueFromArray(insertX);
