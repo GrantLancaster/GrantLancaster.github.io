9adc6213a8bf082681b8eb82a0f5c4fcee35494b
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2022-11-19 09:02:32 -0700

    Update Index.js

--- FinalProject/Index.js
+++ FinalProject/Index.js
@@ -32,24 +32,40 @@ class Player {
             height: 100
             }
         this.velocity = {
-            x: 1,
-            Xmax: 10,
-            y: 1,
-            Ymax: 10
+            x: 0,
+            Xmax: 7,
+            y: 0,
+            Ymax: 7
             }
         this.sides = {
             right: this.position.x + this.size.width,
             bottom: this.position.y + this.size.height
             }
-        this.acceleration = {
+        this.acceleration = { //This is acceleration due to gravity
             x: 2,
             y: 2
             }
+        this.keys ={
+             ArrowUp: { //Up Arrow not pressed by default
+                pressed: false
+            },
+            ArrowDown: {
+                pressed: false
+            },
+            ArrowRight: {
+                pressed: false
+            },
+            ArrowLeft: {
+                pressed: false
+            }
+            }
         }
+
     draw() {
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
-    }
+    }//End of Draw Method
+
     gravity() {
         this.position.y += this.acceleration.y;
         this.sides.bottom = this.position.y + this.size.height; //Gotta make sure you update the variable each time through the loop
@@ -58,63 +74,62 @@ class Player {
         } else {
             this.acceleration.y =0;
         }
-    }
+    }//End of 'gravity' Method
+
     update() {
         //this.gravity(); //force it to have gravity
-    }
+        this.move();
+    }//End of 'Update' Method
 
-    arrowKeysDown() {
+    move() {
+        if (this.keys.ArrowUp.pressed) {
+            this.velocity.y = -(this.velocity.Ymax);
+        }else if (this.keys.ArrowDown.pressed) {
+            this.velocity.y = this.velocity.Ymax;
+        }else if (this.keys.ArrowLeft.pressed) {
+            this.velocity.x = -(this.velocity.Xmax);
+        }else if (this.keys.ArrowRight.pressed) {
+            this.velocity.x = this.velocity.Xmax;
+        }else{
+            this.velocity.x = 0;
+            this.velocity.y = 0;
+        }
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
         window.addEventListener("keydown", (e) => {
             switch (e.key) {
                 case "ArrowUp":
-                    this.position.y -= (this.velocity.y * this.acceleration.y);
-                    if (this.velocity.y < this.velocity.Ymax) {
-                        this.velocity.y++;
-                    } else this.velocity.y = this.velocity.Ymax;
+                    this.keys.ArrowUp.pressed = true;
                     break;
-
                 case "ArrowRight":
-                    this.position.x += (this.velocity.x * this.acceleration.x);
-                    if (this.velocity.x < this.velocity.Xmax) {
-                        this.velocity.x++;
-                    } else this.velocity.x = this.velocity.Xmax;
-                    break;
-
+                    this.keys.ArrowRight.pressed = true;
+                    break;  
                 case "ArrowDown":
-                    this.position.y += (this.velocity.y * this.acceleration.y);
-                    if (this.velocity.y < this.velocity.Ymax) {
-                        this.velocity.y++;
-                    } else this.velocity.y = this.velocity.Ymax;
+                    this.keys.ArrowDown.pressed = true;
                     break;
-                
                 case "ArrowLeft":
-                    this.position.x -= (this.velocity.x * this.acceleration.x);
-                    if (this.velocity.x < this.velocity.Xmax) {
-                        this.velocity.x++;
-                    } else this.velocity.x = this.velocity.Xmax;
+                    this.keys.ArrowLeft.pressed = true;
                     break;
             }
         })
-    }
-
-    arrowKeysUp() {
         window.addEventListener("keyup", (e) => {
             switch (e.key) {
                 case "ArrowUp":
-                    this.velocity.y = 1;
+                    this.keys.ArrowUp.pressed = false;
                     break;
                 case "ArrowRight":
-                    this.velocity.x = 1;
-                    break;
+                    this.keys.ArrowRight.pressed = false;
+                    break;  
                 case "ArrowDown":
-                    this.velocity.y = 1;
+                    this.keys.ArrowDown.pressed = false;
                     break;
                 case "ArrowLeft":
-                    this.velocity.x = 1;
+                    this.keys.ArrowLeft.pressed = false;
                     break;
             }
         })
-    }
+    } //End of 'Move' Method
+    
 }//end of Player class
 
 const playArea = new GameBoard(canvas, ctx, 500, 700, "pink");
@@ -128,5 +143,3 @@ function animate() {
 }
 console.log(player.sides.bottom);
 animate();
-player.arrowKeysDown();
-player.arrowKeysUp();
