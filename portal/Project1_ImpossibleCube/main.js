ba87409acb1b5beeee2ead68aa2b8007c44ab9ed
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2023-10-19 17:52:50 -0600

    Many, Many Changes
    Mostly a lot of work on project 1 and how it's progressing

--- portal/Project1_ImpossibleCube/main.js
+++ portal/Project1_ImpossibleCube/main.js
@@ -1,4 +1,10 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+function render() {
+	renderer.render(scene, camera);
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -8,24 +14,52 @@ const color = 0xFFFFFF;
 const intensity = 3;
 const light = new THREE.DirectionalLight(color, intensity);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({color: 0xB84802});
-const cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BufferGeometry();
+const verticies = new Float32Array([
+	-1.0, -1.0,  -1.0, // v0
+	 1.0, -1.0,  -1.0, // v1
+	 1.0,  1.0,  -1.0, // v2
+
+	 1.0,  1.0,  -1.0, // v3
+	-1.0,  1.0,  -1.0, // v4
+	-1.0, -1.0,  -1.0  // v5
+]);
+
+const NewPlane = new THREE.Vector3(-1, 0, 0);
+
+geometry.setAttribute('position', new THREE.BufferAttribute(verticies, 3));
+const material = new THREE.MeshBasicMaterial({color: 0xB84802, side: THREE.DoubleSide});
+const material2 = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide});
+/*--------*/
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.addEventListener('change', render);
+	controls.update();
+/*-----------*/
+const mesh = new THREE.Mesh(geometry, material);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const loader = new GLTFLoader().setPath("../models/");
+let meesh;
+loader.load('test.gltf', function ( gltf ) {
+	meesh = gltf.scene;
+	scene.add(meesh);
+	renderer.render(scene, camera);
+})
 
 camera.position.z = 2;
 
+meesh.position.set(3, 3, 3);
 light.position.set(-1, 2, 4);
 scene.add(light);
-scene.add(cube);
+scene.add(mesh);
+
 
 function animate() {
-    cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.01;
 	renderer.render(scene, camera);
-	requestAnimationFrame(animate);
+
 }
 animate();
