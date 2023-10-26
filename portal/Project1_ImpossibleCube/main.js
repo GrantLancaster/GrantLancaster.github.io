769244607efff926ced8eb4921fa83120306d10c
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2023-10-26 13:44:07 -0600

    Update main.js
    A lot of cleaning up and organizing my code

--- portal/Project1_ImpossibleCube/main.js
+++ portal/Project1_ImpossibleCube/main.js
@@ -2,70 +2,76 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-function render() {
-	renderer.render(scene, camera);
-}
-
+/*----- Camera, Scene, Renderer Setup -----*/
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+camera.position.z = 10;
+/*-----------------------------------------*/
+
+/*------------- Import Loader -------------*/
+const loader = new GLTFLoader();
+
+loader.load('models/LargerCube.gltf', process);
+loader.load('models/test.gltf', process);
+/*-----------------------------------------*/
+
+/*------------ Materials Setup ------------*/
 const color = 0xFFFFFF;
 const intensity = 3;
 const light = new THREE.DirectionalLight(color, intensity);
 
-const Plane = new THREE.PlaneGeometry(6, 6);
-const material3 = new THREE.MeshBasicMaterial({color: 0x00AEFF, side: THREE.DoubleSide});
-
-const geometry = new THREE.BufferGeometry();
-const verticies = new Float32Array([
-	-1.0, -1.0,  -1.0, // v0
-	 1.0, -1.0,  -1.0, // v1
-	 1.0,  1.0,  -1.0, // v2
-
-	 1.0,  1.0,  -1.0, // v3
-	-1.0,  1.0,  -1.0, // v4
-	-1.0, -1.0,  -1.0  // v5
-]);
-
-
-geometry.setAttribute('position', new THREE.BufferAttribute(verticies, 3));
-const material = new THREE.MeshBasicMaterial({color: 0xB84802, side: THREE.DoubleSide});
 const material2 = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide});
-/*--------*/
+const material3 = new THREE.MeshBasicMaterial({color: 0x00AEFF, side: THREE.DoubleSide});
+/*-----------------------------------------*/
+
+/*----------- Geometries Setup ------------*/
+const Plane = new THREE.PlaneGeometry(6, 6);
+const SphereGeo = new THREE.SphereGeometry(0.1);
+/*-----------------------------------------*/
+
+/*------ Mesh Initialization Setup --------*/
+const Sphere = new THREE.Mesh(SphereGeo, material2);
+const newPlane = new THREE.Mesh(Plane, material3);
+
+const model = new THREE.Object3D();
+const smallCube = new THREE.Object3D();
+
+let models = [model, smallCube];
+/*-----------------------------------------*/
+
+/*------------ Orbit Controls -------------*/
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.addEventListener('change', render);
 	controls.update();
-/*-----------*/
-const mesh = new THREE.Mesh(geometry, material);
-const newPlane = new THREE.Mesh(Plane, material3);
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const loader = new GLTFLoader();
-const model = new THREE.Object3D();
-loader.load('models/LargerCube.gltf', process);
-
-camera.position.z = 10;
+/*-----------------------------------------*/
 
 light.position.set(1, 2, 4);
-scene.add(light);
-scene.add(mesh);
-scene.add(newPlane);
-newPlane.position.z = 3.9;
-newPlane.position.y = 3;
+newPlane.position.set(0, 0, -3);
 
 
-function animate() {
-    mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.01;
+function render() {
 	renderer.render(scene, camera);
 }
+
+function animate() {
+	scene.add(light);
+	scene.add(Sphere);
+	scene.add(newPlane);
+	renderer.render(scene, camera);
+}
+
 function process(gltf) {
-	gltf.scene.position.set(0, 0, 1);
-	gltf.scene.scale.set(2, 2, 2);
-	model.add(gltf.scene);
-	scene.add(model);
+	for (let i = 0; i < models.length; i++) {
+		gltf.scene.position.set(0, -3, 0);
+		gltf.scene.scale.set(2, 2, 2);
+		models[i].add(gltf.scene);
+		scene.add(models[i]);
+	}
+	renderer.render(scene, camera);
+
 }
 animate();
