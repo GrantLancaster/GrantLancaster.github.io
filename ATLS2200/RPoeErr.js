a6bb4747426b9435756bdcddf87f1f7221b205a5
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2023-12-15 23:51:51 -0700

    test code

--- ATLS2200/RPoeErr.js
+++ ATLS2200/RPoeErr.js
@@ -1,4 +1,15 @@
-const article = document.getElementById("article");
-article.innerHTML += 2;
-console.log(article.textContent);
-console.log(typeof article.textContent);
+const newBtn = document.querySelector("#button");
+const numBox = document.querySelector(".number");
+let length = 9;
+
+
+function getRandom(length) {
+    let number = Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, length-1));
+    return number;
+}
+function displayNumber() {
+    let phoneNumber = getRandom(length)
+    numBox.textContent = phoneNumber;
+}
+
+newBtn.addEventListener("click", displayNumber)
--- Script/modules/info.js
+++ Script/modules/info.js
@@ -1,54 +1,87 @@
-const projects = {
-    "impossibleCube": {
-        "name": "Impossible Cube",
-        "description": `An exploration of non-euclidean geometries by creating impossible
-        situation in form that we are familiar with. Built with Three.js and vite, the project
-        gives users a place to explore part of a mind-bending reality that is unkown to us.`,
-        "dates": "Fall 2023",
-        "institution": "University of Colorado Boulder",
-        "collaborators": "N/A",
-        "hosting professor": "Anthony Pinter",
-        "livePage": "https://grantishdesigns.me/portal/Project1_ImpossibleCube/dist/index.html"
+const grantL = {
+    "aboutme":``,
+    "resume":{
+        "desiredjob": ["Front End Web Developer", "UX Designer", "Back-End Web Developer", "Full-Stack Developer", "Analytical Chemist"],
+        "skills":{
+            "hard": [],
+            "soft": []
+        },
+        "awards": {},
+        "experience": {
+            "serviceindustry": {},
+            "chemistry": {},
+            "learningassistant": {},
+            "programming": {},
+            "webdev": {},
+            "communityservice": {}
+        },
+        "education": {
+            "highschool": "",
+            "texastech": "",
+            "frontrangecommunitycollege": "",
+            "cuboulder": ""
+        },
+        "links": {
+            "github": "",
+            "linkedin": "",
+            "portfoliosite": "",
+            "twitchtv": ""
+        },
+        "references": {}
     },
+    "projects": {
+        "impossibleCube": {
+            "name": "Impossible Cube",
+            "description": `An exploration of non-euclidean geometries by creating impossible
+            situation in form that we are familiar with. Built with Three.js and vite, the project
+            gives users a place to explore part of a mind-bending reality that is unkown to us.`,
+            "dates": "Fall 2023",
+            "institution": "University of Colorado Boulder",
+            "collaborators": "N/A",
+            "hostingprofessor": "Anthony Pinter",
+            "livePage": "https://grantishdesigns.me/portal/Project1_ImpossibleCube/dist/index.html"
+        },
 
-    "nexusPlay": {
-        "name": "Nexus Play",
-        "description": "",
-        "dates": "Fall 2023",
-        "institution": "University of Colorado Boulder",
-        "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
-        "hosting professor": "N/A",
-        "livePage": "http://grantishdesigns.me/portal"
+        "nexusPlay": {
+            "name": "Nexus Play",
+            "description": "",
+            "dates": "Fall 2023",
+            "institution": "University of Colorado Boulder",
+            "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
+            "hostingprofessor": "N/A",
+            "livePage": "http://grantishdesigns.me/portal"
+        },
+
+        "galaga": {
+            "name": "Really Bad Galaga",
+            "description": "",
+            "dates": "Fall 2023",
+            "institution": "University of Colorado Boulder",
+            "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
+            "hostingprofessor": "N/A",
+            "livePage": "http://grantishdesigns.me/portal"
+        },
+
+        "audioTurtle": {
+            "name": "Audio Turtle",
+            "description": "",
+            "dates": "Fall 2023",
+            "institution": "University of Colorado Boulder",
+            "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
+            "hostingprofessor": "N/A",
+            "livePage": "http://grantishdesigns.me/portal"
+        },
+
+        "dataDice": {
+            "name": "Data Dice",
+            "description": "",
+            "dates": "Fall 2023-Spring 2024",
+            "institution": "University of Colorado Boulder",
+            "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
+            "hostingprofessor": "N/A",
+            "livePage": "http://grantishdesigns.me/portal"
+        }
     },
-
-    "galaga": {
-        "name": "Really Bad Galaga",
-        "description": "",
-        "dates": "Fall 2023",
-        "institution": "University of Colorado Boulder",
-        "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
-        "hosting professor": "N/A",
-        "livePage": "http://grantishdesigns.me/portal"
-    },
-
-    "audioTurtle": {
-        "name": "Audio Turtle",
-        "description": "",
-        "dates": "Fall 2023",
-        "institution": "University of Colorado Boulder",
-        "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
-        "hosting professor": "N/A",
-        "livePage": "http://grantishdesigns.me/portal"
-    },
-
-    "dataDice": {
-        "name": "Data Dice",
-        "description": "",
-        "dates": "Fall 2023-Spring 2024",
-        "institution": "University of Colorado Boulder",
-        "collaborators": "Jyotish Vardhan, Chin-Hsuan (Andrea) Wang",
-        "hosting professor": "N/A",
-        "livePage": "http://grantishdesigns.me/portal"
-    }
+    "contact": "grant.lancaster3@gmail.com"
 }
-export default projects;
+export default grantL;
