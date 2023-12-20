678effa9f7bd93e6fa94a324f3367cc99c5df3d5
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2023-12-19 22:59:08 -0700

    Even more portfolio updates

--- Script/modules/homeScreen.js
+++ Script/modules/homeScreen.js
@@ -3,10 +3,14 @@ let homescreen = `
 <header>
     <div id="name">
         <p class="filled" id="firstName">Grant</p>
-        <hp class="stroke" id="lastName">Lancaster</p>
+        <p class="stroke" id="lastName">Lancaster</p>
     </div>
     <p id="subHeader">Developer | Designer | <span id="Abutton" class="controllerButtons">A</span> <span id="Bbutton" class="controllerButtons">B</span> <span id="Xbutton" class="controllerButtons">X</span> <span id="Ybutton" class="controllerButtons">Y</span> Enthusiast</p>
 </header>
+
+<div id="quoteParent">
+    <h5 id="quote">Let's Build New Worlds!</h5>
+</div>
 `
 
 export default homescreen;
--- Script/modules/links.js
+++ Script/modules/links.js
@@ -3,9 +3,15 @@ import grantL from "./info.js";
 
 let links =    `
     <div class="linkParent">
-        <img class="icons" src="../../img/email-64.png" alt="email icon">
-        <img class="icons" src="../../img/github-mark-white.png" alt="github logo">
-        <img id="linkedin" class="icons" src="../../img/LI-In-Bug.png" alt="linked in logo">
+        <a href="https://drive.google.com/file/d/1uUbULLOe9_A59Vvdxv8dZh_oPcJWH4YI/view?usp=sharing" target="_blank" rel="noreferrer noopener">
+            <img class="icons" src="../../img/view-details-64.png" alt="email icon">
+        </a>
+        <a href="https://github.com/GrantLancaster" target="_blank" rel="noreferrer noopener">
+            <img class="icons" src="../../img/github-mark-white.png" alt="github logo">
+        </a>
+        <a href="https://www.linkedin.com/in/grant1shdesigns/" target="_blank" rel="noreferrer noopener">
+            <img class="icons" src="../../img/LI-In-Bug.png" alt="linked in logo">
+        </a>
     </div>
 `
 
