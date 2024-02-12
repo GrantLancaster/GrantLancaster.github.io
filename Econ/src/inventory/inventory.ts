a361ee4671acb9780a50a31f4d7d20030501f2e0
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2024-02-12 15:51:58 -0700

    Worked on some ECON stuff

---
+++ Econ/src/inventory/inventory.ts
@@ -0,0 +1,20 @@
+function collect(material:string, amount:number) {
+    switch (material) {
+        case "wood": {
+
+            break
+        }
+        case "stone": {
+
+            break
+        }
+    }
+}
+
+const build =`
+    <div class="inventoryBox">
+        <h5>Resources:</h5>
+        <p>Wood: </p>
+        <p>Stone: </p>
+    </div>`
+export default build
---
+++ Econ/src/map/buildMap.ts
@@ -0,0 +1,15 @@
+import "./buildMap.css"
+
+
+function buildMap() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    console.log(width, height);
+}
+
+const tiles = `
+    <div class="woodBlock"></div>
+    <div class="stoneBlock"></div>
+`
+export default tiles
+
