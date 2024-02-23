ce168825771ff87f2181f4dde27ca41c343fe551
Grantish <94849378+GrantLancaster@users.noreply.github.com>
2024-02-23 10:12:04 -0700

    Adding to Info.TS

--- TYPOLab/src/components/info.ts
+++ TYPOLab/src/components/info.ts
@@ -1,3 +1,68 @@
 export const TYPOLab: object = {
-    "Test": {"jsut": "Just more tstinsglksndkl"}
+    "Sections": {
+        "About": {
+            "Copy": {
+                "TLDR": "TL;DR: TYPO Lab is a design lab doing weird things with text-based technologies.",
+                "Details": ["Located on the third floor of the ATLAS/Roser Building, the TYPO Lab offers the space, technical resources, and faculty support to promote creative work and practice-based research into text-based technologies. The lab funds undergraduate and graduate research positions, provide hands-on access to historical and contemporary text-based technologies, and run thematic and technical workshops and demonstrations for the CU Boulder community.",
+
+                "TYPO Lab explores the materiality of language: the way that the structures, forms, and physical inscription technologies of language play an active role in the construction of meaning. The lab offer access to a wide range of technologies related to text. From a movable type letterpress to state-of-the-art computer workstations equipped for machine learning projects, it facilitates the exploration of text-based technologies from historically broad and interdisciplinary perspectives.", 
+
+                "“Typo” is an abbreviation for typography, but it also signifies a typographic error or mistake. Our lab logo, based on the form of an insertion edit mark used in proofreading, is a visual reminder of the generative potential of errors and mistakes. The TYPO Lab frames the errors, glitches, and mistakes of text-based technology—and technology in general—as a locus of creative inquiry and research. We rarely notice technology when it operates as intended and the lab welcomes unconventional and unorthodox use cases that can illuminate its limitations and affordances. Further TYPO Lab seeks to destabilize these technologies to uncover the cultural biases and norms frequently embedded within their construction. TYPO Lab seeks to be a diverse and inclusive home for creative, critical, and speculative design work that transcends disciplinary perspectives and boundaries."]
+            }
+        },
+        "Projects": {},
+        "People": {
+            0: {
+                "Name": "Joel Swanson",
+                "Role": "Lab Director",
+                "PersonalSite": "joelswanson.art",
+                "Email": "joel.swanson@colorado.edu",
+                "Image": ""
+            },
+            1: {
+                "Name": "Anna Lowrimore",
+                "Role": "Lab Assistant",
+                "PersonalSite": "",
+                "Email": "",
+                "Image": ""
+            },
+            2: {
+                "Name": "Ian Hatcher",
+                "Role": "PhD Student",
+                "PersonalSite": "",
+                "Email": "",
+                "Image": ""
+            },
+            3: {
+                "Name": "Elodie Thelliez",
+                "Role": "Lab Assistant",
+                "PersonalSite": "",
+                "Email": "",
+                "Image": ""
+            },
+            4: {
+                "Name": "Jason Fontillas",
+                "Role": "Lab Assistant",
+                "PersonalSite": "",
+                "Email": "",
+                "Image": ""
+            },
+            5: {
+                "Name": "Brad Gallagher",
+                "Role": "PhD Student",
+                "PersonalSite": "",
+                "Email": "",
+                "Image": ""
+            }
+
+        },
+        "Courses": {},
+        "Contact": {}
+    },
+    "Summary": "A speculative design lab critically exploring the intersection of text and technology.",
+    "InstagramLink": "",
+    "Address": ["ATLAS Institute", "University of Colorado Boulder", "Roser ATLAS Center", "1125 18th St. 320 UCB", "Boulder, CO 80309-0320"],
+    "LabLogo": "",
+    "ATLASLogo": "",
+    "CULogo": ""
 }
