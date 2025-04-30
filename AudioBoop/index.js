7b992af5295c3d13ef9bbeb630cfa8d506fd3023
Grant Lancaster <grant.lancaster3@gmail.com>
2025-04-29 18:43:56 -0600

    Some reworks to the animation and added more user interaction. client can now change the color pattern and number of partitions.
    the animation also will alternate between the
    same direction pattern and alternating pattern
    after it hits a distance threshold from the origin.

--- AudioBoop/index.js
+++ AudioBoop/index.js
@@ -13,12 +13,26 @@ const data = {
   alternate_direction: false,
   rotating_canvas: false,
   canvas_rot_increment: 1,
+  gradient: {
+    active: false,
+    r_decrement: 0,
+    g_decrement: 0,
+    b_decrement: 0,
+  },
+  color: {
+    r: Math.floor(Math.random() * 255),
+    g: Math.floor(Math.random() * 255),
+    b: Math.floor(Math.random() * 255),
+  },
 }
 
 const startButton = document.querySelector("#start");
 startButton.addEventListener("click", ()=>{
   data.running = true;
-  if (!data.initialized) {initialize(); data.initialized = true;}
+  if (!data.initialized) {
+    initialize();
+    data.initialized = true;
+  }
   animate();
 });
 
@@ -43,11 +57,26 @@ canvas_rot.addEventListener("change", (e)=>{
   } else {
     data.rotating_canvas = false;
   }
+});
+
+const gradient_box = document.querySelector("#gradient");
+gradient_box.addEventListener("change", (e)=>{
+  if (e.target.checked) {
+    data.gradient.active = true;
+  } else {
+    data.gradient.active = false;
+  }
+});
+
+const partition_box = document.querySelector("#partitions");
+partition_box.addEventListener("change", (e)=>{
+  data.partitions = e.target.value || 16;
 })
 
 
 class Circle {
-  constructor(x,y,radius, angle, color) {
+  constructor(x,y,radius, angle, color, id) {
+    this.id = id;
     this.x = x;
     this.y = y;
     this.radius = radius;
@@ -63,7 +92,7 @@ class Circle {
     ctx.fill();
   }
   update() {
-    //this.check_boundries();
+    this.check_boundries();
     this.move();
     this.increase_radius();
     this.draw();
@@ -71,37 +100,52 @@ class Circle {
   move() {
     this.angle += 1;
     this.dist += 0.2;
-    this.y = canvas.height/2 + (this.dist *this.direction) * Math.cos( (this.angle*Math.PI)/180 );
+    this.y = canvas.height/2 + (this.dist * this.direction) * Math.cos( (this.angle*Math.PI)/180 );
     this.x = canvas.width/2 + this.dist * Math.sin( (this.angle*Math.PI)/180 );
   }
   increase_radius() {
     //this.radius += 0.005;
-    this.radius += this.dist/10000
+    //this.radius += 0.002;
   }
   check_boundries() {
-    if (this.x > canvas.width + this.radius) {
-      this.x = 0 ;
-    } else if (this.x < 0 - this.radius) {
-      this.x = canvas.width;
-    }
-    if (this.y > canvas.height + this.radius) {
-      this.y = 0;
-    } else if (this.y < 0 - this.radius) {
-      this.y = canvas.height;
+    if (this.dist > 300) {
+      this.dist = 0;
+      if (this.id % 2 == 0) {
+        this.direction *= -1;
+      }
     }
   }
 }
 
+function reroll_colors() {
+  data.color.r = Math.floor(Math.random() * 255);
+  data.color.g = Math.floor(Math.random() * 255);
+  data.color.b = Math.floor(Math.random() * 255);
+}
+
 function initialize() {
+  let increment = 360 / data.partitions;
+  data.gradient.r_decrement = Math.floor(data.color.r / data.partitions);
+  data.gradient.g_decrement = Math.floor(data.color.g / data.partitions);
+  data.gradient.b_decrement = Math.floor(data.color.b / data.partitions);
+
   for (let shape=0; shape<data.partitions; shape++) {
-    let increment = 360 / data.partitions;
+    if (!data.gradient.active) {
+      reroll_colors();
+    } else {
+      data.color.r += data.gradient.r_decrement * shape;
+      data.color.g += data.gradient.g_decrement * shape;
+      data.color.b += data.gradient.b_decrement * shape;
+    }
     const circle = new Circle(
       canvas.width/2,
       canvas.height/2,
       6,
       increment * shape,
-      `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+      `rgb(${data.color.r},${data.color.g},${data.color.b})`,
+      shape
     );
+
 
     if (data.alternate_direction) {
       if (shape%2 ==0 ) {
@@ -113,6 +157,7 @@ function initialize() {
 }
 
 function animate() {
+  
   for ( let obj of data.shapes) {
     obj.update();
   }
